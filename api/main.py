59439f27e31f7d87d949ad84ea7ab9c7ba90e13d
sinbumu <sinbumu@gmail.com>
2025-07-13 21:33:48 +0900

    Update main.py
    cors 관련 추가

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from model_loader import model
 import xgboost as xgb
 import pandas as pd
@@ -17,6 +18,23 @@ app = FastAPI(
     description="API for real-time fraud risk scoring and model monitoring.",
     version="1.0.0"
 )
+
+# --- CORS Middleware Settings ---
+# 프론트엔드 애플리케이션의 주소를 허용합니다.
+origins = [
+    "https://phenomenal-alfajores-024c07.netlify.app",
+    "http://localhost", # 로컬 테스트용
+    "http://localhost:3000", # 로컬 테스트용 (React 기본 포트)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # 모든 HTTP 메소드 허용
+    allow_headers=["*"], # 모든 HTTP 헤더 허용
+)
+
 
 # --- Endpoints ---
 
